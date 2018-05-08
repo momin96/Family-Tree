653020fdd7e86a54a442562bb580c65c0a749c37
Nasir <momin96@gmail.com>
2018-05-08 17:09:54 +0530

    New View files added

---
+++ iOS /Family Tree iOS/NSRTableHeaderView.swift
@@ -0,0 +1,39 @@
+//
+//  NSRTableHeaderView.swift
+//  Family Tree iOS
+//
+//  Created by Nasir Ahmed Momin on 08/05/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import UIKit
+
+let BUTTON_HEIGHT = 40.0
+
+class NSRTableHeaderView: UIView {
+
+    @IBOutlet weak var headerTitle: UILabel!
+    
+    
+    override func draw(_ rect: CGRect) {
+        
+        let nameRect: CGRect = CGRect(x: 0, y: headerTitle.bounds.height, width: self.bounds.width / 2, height: BUTTON_HEIGHT)
+        let ageRect: CGRect = CGRect(x: self.bounds.width / 2, y: headerTitle.bounds.height,, width: self.bounds.width / 2, height: BUTTON_HEIGHT) as!
+        
+        let nameButtonView = NSRHeaderButton(frame: nameRect)
+        self.addSubview(nameButtonView)
+        
+        
+        let ageButtonView = NSRHeaderButton(frame: ageRect)
+        self.addSubview(ageButtonView)
+        
+    }
+}
+
+
+class NSRHeaderButton: UIView {
+    
+    @IBOutlet weak var button:          UIButton!
+    @IBOutlet weak var orderImageView:  UIImageView!
+    
+}
