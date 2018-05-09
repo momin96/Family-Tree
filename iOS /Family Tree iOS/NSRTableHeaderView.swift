f9100f8a02567517f38fb8375fcf5200d4d24c90
Nasir <momin96@gmail.com>
2018-05-09 09:40:20 +0530

    Removed unused code & UI, documentation

--- iOS /Family Tree iOS/NSRTableHeaderView.swift
+++ iOS /Family Tree iOS/NSRTableHeaderView.swift
@@ -10,28 +10,42 @@ import UIKit
 
 let BUTTON_HEIGHT = 45.0
 
+/**
+ This class is responsible for rendering Header view for table
+ */
 class NSRTableHeaderView: UITableViewHeaderFooterView {
 
+    /// title for table view
     @IBOutlet weak var headerTitle: UILabel!
+    
+    /// Sort by Name button
     @IBOutlet weak var nameSort: UIButton!
+    
+    /// Sort by Age button
     @IBOutlet weak var ageSort: UIButton!
     
+    /// An instance to perform sorting operation via delegate to class who confirms it.
     var delegate: SortFamilyMemberProtocol?
 
     
+    // MARK: Target Action methods
+    
+    /**
+     Perform sort operation Member's Name
+     
+     - Parameter sender: An instance to hold status like selected & any others
+     */
     @IBAction func sortByName(_ sender: UIButton) {
         delegate?.sortByAge!(sender)
     }
     
+    /**
+     Perform sort operation Member's age
+
+     - Parameter sender: An instance to hold status like selected & any others
+     */
     @IBAction func sortByAge(_ sender: UIButton) {
         delegate?.sortByAge!(sender)
     }
 }
 
-
-class NSRHeaderButton: UIView {
-    
-    @IBOutlet weak var button:          UIButton!
-    @IBOutlet weak var orderImageView:  UIImageView!
-    
-}
